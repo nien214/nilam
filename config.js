5f0b6642691b297c1a35eff579a02bc953cc6a1b
Tan Kuan Nien <vincenttan89@gmail.com>
2026-02-22 17:12:27 +0800

    anonkey apply

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ window.NILAM_CONFIG = {
   // Contoh: ""
   supabaseUrl: "https://myvnrqdronojfqkcuonx.supabase.co",
   // Kunci anon public Supabase
-  supabaseAnonKey: "sb_publishable_0Hi4S2egX92XkeSREKzKaA_75j13v8T",
+  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im15dm5ycWRyb25vamZxa2N1b254Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3NDU1NDgsImV4cCI6MjA4NzMyMTU0OH0.ibgx2ggHvZQMeu9iK0QjPkYrDXBYuN8NO1B4o6qMGrI",
 };
