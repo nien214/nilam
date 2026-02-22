04bf351efb86f3b17fb4c3362511482c74a1fc42
Tan Kuan Nien <vincenttan89@gmail.com>
2026-02-22 17:07:40 +0800

    supabase added

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.NILAM_CONFIG = {
-  // Contoh: "https://xxxx.supabase.co"
-  supabaseUrl: "",
+  // Contoh: ""
+  supabaseUrl: "https://myvnrqdronojfqkcuonx.supabase.co",
   // Kunci anon public Supabase
-  supabaseAnonKey: "",
+  supabaseAnonKey: "sb_publishable_0Hi4S2egX92XkeSREKzKaA_75j13v8T",
 };
